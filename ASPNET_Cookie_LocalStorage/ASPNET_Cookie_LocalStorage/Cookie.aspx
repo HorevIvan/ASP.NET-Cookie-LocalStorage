69aa391841a5961720b8f51028e3b2e3d453e4db
horevivan <horevivan@yandex.ru>
2014-09-24 16:13:44 +0800

    Cookie

---
+++ ASPNET_Cookie_LocalStorage/ASPNET_Cookie_LocalStorage/Cookie.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Cookie.aspx.cs" Inherits="ASPNET_Cookie_LocalStorage.Cookie" %>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head runat="server">
+        <title></title>
+    </head>
+    <body>
+        <form id="CookieForm" runat="server">
+            <% if (!IsKnownUser) { %>
+            <div>  
+                Я тебя не знаю, представься:  
+                <asp:TextBox runat="server" ID="NameTextBox" /><br />
+                <asp:Button runat="server" ID="EnterButton" Text="Послать" OnClick="EnterButton_Click" />
+            </div>
+            <% } else { %>
+                Привет <%=UserName %><br />
+                знаю тебя до: <%=UserCookieExpiries.ToLongDateString() %>  <%=UserCookieExpiries.ToLongTimeString() %>
+            <% } %>
+        </form>
+    </body>
+</html>
---
+++ ASPNET_Cookie_LocalStorage/ASPNET_Cookie_LocalStorage/Cookie.aspx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASPNET_Cookie_LocalStorage
+{
+    public partial class Cookie : System.Web.UI.Page
+    {
+        public static readonly String UserCookie = "user";
+
+        public static readonly String UserNameCookie = "name";
+
+        public Boolean IsKnownUser { private set; get; }
+
+        public String UserName { private set; get; }
+
+        public DateTime UserCookieExpiries { private set; get; }
+
+        protected void Page_Load(Object sender, EventArgs e)
+        {
+            //if (!IsPostBack)
+            {
+                ReadCookie();
+            }
+        }
+
+        protected void EnterButton_Click(object sender, EventArgs e)
+        {
+            WriteCookie();
+        }
+        
+        private void ReadCookie()
+        {
+            var userCookie = Request.Cookies["user"];
+
+            if (userCookie != null && userCookie.HasKeys)
+            {
+                IsKnownUser = true;
+
+                UserName = userCookie["name"];
+
+                UserCookieExpiries = userCookie.Expires;
+            }
+            else
+            {
+                IsKnownUser = false;
+
+                UserName = null;
+
+                UserCookieExpiries = DateTime.MinValue;
+            }
+        }
+
+        private void WriteCookie()
+        {
+            if (NameTextBox.Text.Length > 0)
+            {
+                var userCookie = Response.Cookies["user"];
+
+                userCookie["name"] = NameTextBox.Text;
+
+                userCookie.Expires = DateTime.Now.AddDays(1);
+            }
+        }
+    }
+}
